f9b065f257c8327588e55c79ca26968ecd868f62
Rohit Sharma <rohit.sharma.e21@nsut.ac.in>
2024-06-20 09:51:32 +0530

    new change

---
+++ G1/rotate_matrix.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void print(vector<vector<int> > matrix, int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cout<<matrix[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+void rotate_matrix(vector<vector<int> > &matrix, int n){
+    vector<vector<int> > rotate(n,vector<int>(n,0));
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            rotate[i][n-j-1] = matrix[j][i];
+        }
+    }
+    // print(rotate,n);
+    matrix = rotate;
+}
+
+
+int main(){
+    int n=6;
+    cin>>n;
+    vector< vector< int > >matrix(n,vector<int>(n));
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cin>>matrix[i][j];
+        }
+    }
+    cout<<"original matrix: ";
+    print(matrix, n);
+    cout<<endl<<"rotated matrix: ";
+
+    rotate_matrix(matrix,n);
+    print(matrix,n);
+
+    return 0;
+
+
+    
+    // 1 2 3
+    // 4 5 6
+    // 7 8 9
+
+}
