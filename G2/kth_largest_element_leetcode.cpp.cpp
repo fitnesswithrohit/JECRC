9b1de575ca1d889fa6a54e4d748d78d7e448f80f
Rohit Sharma <rohit.sharma.e21@nsut.ac.in>
2024-07-03 15:27:01 +0530

    new codess

---
+++ G2/kth_largest_element_leetcode.cpp.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        int n=nums.size();
+        priority_queue <int, vector<int>, greater<int> > pq;
+        for(int i=0;i<n;i++){
+            if(pq.size() < k){
+                pq.push(nums[i]);
+            }
+            else{
+                if(pq.top() < nums[i]){
+                    pq.pop();
+                    pq.push(nums[i]);
+                }
+            }
+        }
+        return pq.top();
+    }
+};
